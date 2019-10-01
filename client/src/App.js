5bff4707af2a8708526b937e41277358291783c3
adishthapa <adishthapa@outlook.com>
2019-10-01 19:56:54 -0400

    Testing isAuthenticated functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { useAuth0 } from "./react-auth0-wrapper";
 import "./App.css";
@@ -7,31 +7,24 @@ import Profile from "./components/Profile";
 import ChatWindow from "./components/Chat";
 import PrivateRoute from "./components/PrivateRoute";
 
-class App extends Component {
-  // const { loading } = useAuth0();
+const App = () => {
+  const { isAuthenticated } = useAuth0();
 
-  // if (loading) {
-  //   return (
-  //     <div>Loading...</div>
-  //   );
-  // }
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        {isAuthenticated && <ChatWindow />}
 
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <header>
-            <NavBar />
-          </header>
-          <ChatWindow />
-          <Switch>
-            <Route path="/" exact />
-            <PrivateRoute path="/profile" component={Profile} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
