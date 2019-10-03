bd802c75416d8db6da6c2f80c2f736a03f6e47ab
Flying Serpent <nickprather11@gmail.com>
2019-10-03 15:14:48 -0400

    changed chat route in App.js and chat styling

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -88,8 +88,7 @@ class ChatWindow extends Component {
               value={this.state.username}
               onChange={this.handleInputChange}
             />
-          </div>
-          <div className="messageDiv">
+
             <input
               key="message"
               type="text"
@@ -99,8 +98,8 @@ class ChatWindow extends Component {
               value={this.state.message}
               onChange={this.handleInputChange}
             />
+            <input type="submit" value="Send" id="chat-submit" onClick={this.sendMessage} />
           </div>
-          <input type="submit" value="Send" id="chat-submit" onClick={this.sendMessage} />
         </form>
       </div>
     );
