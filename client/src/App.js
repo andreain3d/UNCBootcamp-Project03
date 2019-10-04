b9446cd20f1798282dab6ceef52bb5e7974a3250
Eugio <47667122+ejhalpin@users.noreply.github.com>
2019-10-04 10:25:43 -0400

    cards now render through button clicks

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Table from "./components/table";
 import Navbar from "./components/navbar";
 import Chat from "./components/Chat/";
 import Options from "./components/options";
+import io from "socket.io-client";
+import axios from "axios";
 
 const styles = {
   grow: {
@@ -14,15 +16,85 @@ const styles = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerCards: [],
+      flop: [],
+      turn: {},
+      river: {},
+      hands: [],
+      action: 0
+    };
+
+    this.socket = io.connect();
+
+    this.socket.on("ADDPLAYER", data => {
+      console.log(data);
+    });
+
+    this.socket.on("PRIME", data => {
+      console.log(data);
+    });
+
+    this.socket.on("DEALCARDS", data => {
+      console.log(data);
+      axios.get("/api/player/0/cards").then(res => {
+        console.log(res.data.playerCards);
+        this.setState({ playerCards: res.data.playerCards });
+      });
+    });
+
+    this.socket.on("DOFLOP", data => {
+      console.log(data);
+      this.setState({ flop: data.flop });
+    });
+
+    this.socket.on("DOTURN", data => {
+      console.log(data);
+      this.setState({ turn: data.turn });
+    });
+
+    this.socket.on("DORIVER", data => {
+      console.log(data);
+      this.setState({ river: data.river });
+    });
+
+    this.socket.on("CALCULATEHANDS", data => {
+      console.log(data);
+      this.setState({ hands: data.hands });
+    });
+  }
+  primeTable = async () => {
+    for (var i = 0; i < 4; i++) {
+      await axios.post("/api/table/join", {
+        name: `Player_${i}`,
+        cash: 300
+      });
+    }
+    await axios.get("/api/table/prime");
+  };
+
+  nextDeckAction = () => {
+    if (this.state.action > 4) {
+      console.log("HAND OVER");
+      return;
+    }
+    let deckActions = ["deal", "flop", "turn", "river", "hands"];
+    axios.get("/api/table/" + deckActions[this.state.action]).then(res => {
+      this.setState({ action: this.state.action + 1 });
+    });
+  };
   render() {
     const classes = this.props.classes;
     return (
       <Fragment>
         <Navbar />
-        <Table />
+        <Table nextDeckAction={this.nextDeckAction} primeTable={this.primeTable} flop={this.state.flop} turn={this.state.turn} river={this.state.river} />
         <Grid container className={classes.grow}>
           <Grid item xs={12} md={6}>
-            <Options />
+            <Options cards={this.state.playerCards} />
           </Grid>
           <Grid item xs={12} md={6}>
             <Chat />
--- client/src/components/options.js
+++ client/src/components/options.js
@@ -16,46 +16,32 @@ const styles = {
 };
 
 class Options extends Component {
+  constructor(props) {
+    super(props);
+  }
   render(props) {
     const classes = this.props.classes;
+    const { cards } = this.props;
     return (
       <Paper className={classes.grow}>
         <Typography variant="h6">Your Hand</Typography>
         <Grid container justify="center">
-          <PlayerCard />
-          <PlayerCard />
+          <PlayerCard src={cards.length > 0 ? cards[0].frontImage : ""} />
+          <PlayerCard src={cards.length > 0 ? cards[1].frontImage : ""} />
         </Grid>
         <Grid container justify="center">
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.button}
-          >
+          <Button color="primary" variant="contained" className={classes.button}>
             Fold
           </Button>
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.button}
-          >
+          <Button color="primary" variant="contained" className={classes.button}>
             call
           </Button>
-          <Button
-            color="primary"
-            variant="contained"
-            className={classes.button}
-          >
+          <Button color="primary" variant="contained" className={classes.button}>
             raise
           </Button>
         </Grid>
         <Grid container justify="center">
-          <input
-            type="range"
-            min="1"
-            max="100"
-            className={classes.slider}
-            id="myRange"
-          />
+          <input type="range" min="1" max="100" className={classes.slider} id="myRange" />
         </Grid>
       </Paper>
     );
