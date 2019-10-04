04d0e8ac23e8389689025f474d57798b8f6038ba
adishthapa <adishthapa@outlook.com>
2019-10-04 14:46:46 -0400

    Trying to resolve merge issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { useAuth0 } from "./react-auth0-wrapper";
-import "./App.css";
-import NavBar from "./components/NavBar";
-import Profile from "./components/Profile";
-import ChatWindow from "./components/Chat";
-import PrivateRoute from "./components/PrivateRoute";
+import React, { Component, Fragment } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Table from "./components/table";
+import Navbar from "./components/navbar";
+import Chat from "./components/Chat/";
+import Options from "./components/options";
 
-const App = () => {
-  const { isAuthenticated } = useAuth0();
-
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <header>
-          <NavBar />
-        </header>
-        {isAuthenticated && <ChatWindow />}
-        <Switch>
-          <Route path="/" exact />
-          <PrivateRoute path="/profile" component={Profile} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+const styles = {
+  grow: {
+    flexGrow: 1,
+    bottom: 0
+  }
 };
 
-export default App;
+class App extends Component {
+  render() {
+    const classes = this.props.classes;
+    return (
+      <Fragment>
+        <Navbar />
+        <Table />
+        <Grid container className={classes.grow}>
+          <Grid item xs={12} md={6}>
+            <Options />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Chat />
+          </Grid>
+        </Grid>
+      </Fragment>
+    );
+  }
+}
