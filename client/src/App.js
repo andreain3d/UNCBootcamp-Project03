51e45365837b194aaca3446e5dce8a3d4f09c198
adishthapa <adishthapa@outlook.com>
2019-10-05 00:10:03 -0400

    Created basic lobby page with different routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,115 +1,29 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Table from "./components/table";
-import Navbar from "./components/navbar";
-import Chat from "./components/chat";
-import Options from "./components/options";
-import io from "socket.io-client";
-import axios from "axios";
-
-const styles = {
-  grow: {
-    flexGrow: 1,
-    bottom: 0
-  }
-};
+import PrivateRoute from "./components/PrivateRoute";
+import LobbyView from "./pages/LobbyView";
+import TableView from "./pages/TableView";
+import ProfileView from "./pages/ProfileView";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      playerCards: [],
-      flop: [],
-      turn: {},
-      river: {},
-      hands: [],
-      action: 0
-    };
-    //socket should be defined at the top level and passed through to the chat, table, and options components
-    this.socket = io.connect();
-
-    this.socket.on("ADDPLAYER", data => {
-      console.log(data);
-    });
-
-    this.socket.on("PRIME", data => {
-      console.log(data);
-    });
-
-    this.socket.on("DEALCARDS", data => {
-      console.log(data);
-      axios.get("/api/player/0/cards").then(res => {
-        console.log(res.data.playerCards);
-        this.setState({ playerCards: res.data.playerCards });
-      });
-    });
-
-    this.socket.on("DOFLOP", data => {
-      console.log(data);
-      this.setState({ flop: data.flop });
-    });
-
-    this.socket.on("DOTURN", data => {
-      console.log(data);
-      this.setState({ turn: data.turn });
-    });
-
-    this.socket.on("DORIVER", data => {
-      console.log(data);
-      this.setState({ river: data.river });
-    });
-
-    this.socket.on("CALCULATEHANDS", data => {
-      console.log(data);
-      this.setState({ hands: data.hands });
-    });
-  }
-  primeTable = async () => {
-    for (var i = 0; i < 4; i++) {
-      await axios.post("/api/table/join", {
-        name: `Player_${i}`,
-        cash: 300
-      });
-    }
-    await axios.get("/api/table/prime");
-  };
-
-  nextDeckAction = () => {
-    if (this.state.action > 4) {
-      console.log("HAND OVER");
-      return;
-    }
-    let deckActions = ["deal", "flop", "turn", "river", "hands"];
-    axios.get("/api/table/" + deckActions[this.state.action]).then(res => {
-      this.setState({ action: this.state.action + 1 });
-    });
-  };
   render() {
-    const classes = this.props.classes;
     return (
-      <Fragment>
-        <Navbar />
-        <Table
-          socket={this.socket}
-          nextDeckAction={this.nextDeckAction}
-          primeTable={this.primeTable}
-          flop={this.state.flop}
-          turn={this.state.turn}
-          river={this.state.river}
-        />
-        <Grid container className={classes.grow}>
-          <Grid item xs={12} md={6}>
-            <Options socket={this.socket} cards={this.state.playerCards} />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Chat socket={this.socket} />
-          </Grid>
-        </Grid>
-      </Fragment>
+      <BrowserRouter>
+        <Switch>
+          <PrivateRoute path="/table">
+            <TableView />
+          </PrivateRoute>
+          <PrivateRoute path="/profile">
+            <ProfileView />
+          </PrivateRoute>
+          <Route path="/">
+            <LobbyView />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
--- client/src/pages/LobbyView.js
+++ client/src/pages/LobbyView.js
@@ -1,28 +1,34 @@
-// import React from "react";
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import { useAuth0 } from "./react-auth0-wrapper";
-// import NavBar from "./components/NavBar";
-// import Profile from "./components/Profile";
-// import ChatWindow from "./components/Chat";
-// import PrivateRoute from "./components/PrivateRoute";
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import { useAuth0 } from "../react-auth0-wrapper";
 
-// const App = () => {
-//   const { isAuthenticated } = useAuth0();
+const LobbyView = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <header>
-//           <NavBar />
-//         </header>
-//         {isAuthenticated && <ChatWindow />}
-//         <Switch>
-//           <Route path="/" exact />
-//           <PrivateRoute path="/profile" component={Profile} />
-//         </Switch>
-//       </BrowserRouter>
-//     </div>
-//   );
-// };
+  return (
+    <Fragment>
+      <Grid container direction="row" justify="center" alignItems="center">
+        {!isAuthenticated && (
+          <Button onClick={() => loginWithRedirect({})}>Log in</Button>
+        )}
 
-// export default App;
+        {isAuthenticated && (
+          <Button>
+            <Link to="/table">Join Game</Link>
+          </Button>
+        )}
+        {isAuthenticated && (
+          <Button>
+            <Link to="/profile">Profile</Link>
+          </Button>
+        )}
+
+        {isAuthenticated && <Button onClick={() => logout()}>Log out</Button>}
+      </Grid>
+    </Fragment>
+  );
+};
+
+export default LobbyView;
